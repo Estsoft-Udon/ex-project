0342cdaeb04d22691b7238ef32c9fcb04e072a7b
jingeon <ephigh1397@naver.com>
2025-01-11 00:28:38 +0900

    test: Entity For mysql DB Connection test
    Create Entity, RestController, Repository, Service
    for DB Connection test
    Test Result: Connection Working Fine
    
    #5

--- src/main/java/com/estsoft/exproject/controller/rest/UsersController.java
+++ src/main/java/com/estsoft/exproject/controller/rest/UsersController.java
@@ -1,4 +1,23 @@
 package com.estsoft.exproject.controller.rest;
 
+import com.estsoft.exproject.dto.response.UsersResponse;
+import com.estsoft.exproject.entity.Users;
+import com.estsoft.exproject.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/users")
 public class UsersController {
+    private final UsersService usersService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Users> register(@RequestBody Users users) {
+        return ResponseEntity.ok(usersService.register(users));
+    }
 }
--- src/main/java/com/estsoft/exproject/entity/Users.java
+++ src/main/java/com/estsoft/exproject/entity/Users.java
@@ -1,4 +1,62 @@
 package com.estsoft.exproject.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login_id", nullable = false)
+    private String loginId;
+
+//    @Column(nullable = false)
+//    private String name;
+//
+//    @Column(nullable = false)
+//    private String nickname;
+//
+//    @Column(nullable = false)
+//    private String email;
+//
+//    @Column(nullable = false)
+//    private String password;
+//
+//    @Column(name = "created_at", nullable = false)
+//    private LocalDateTime createdAt;
+//
+//    @Column(name = "updated_at")
+//    private LocalDateTime updatedAt;
+//
+//    @Column(name = "deleted_at")
+//    private LocalDateTime deletedAt;
+//
+//    @Column(name = "is_deleted", nullable = false)
+//    private Boolean isDeleted = Boolean.FALSE;
+//
+//    @Column(name = "last_login")
+//    private LocalDateTime lastLoginAt;
+//
+//    @PrePersist
+//    public void prePersist() {
+//        this.createdAt = LocalDateTime.now();
+//        this.updatedAt = LocalDateTime.now();
+//    }
+//
+//    @PreUpdate
+//    public void preUpdate() {
+//        this.updatedAt = LocalDateTime.now();
+//    }
+//
+//    public void updateLastLoginAt() {
+//        this.lastLoginAt = LocalDateTime.now();
+//    }
 }
