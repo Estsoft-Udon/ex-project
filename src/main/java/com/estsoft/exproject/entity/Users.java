ab6348c31e7d47baac9a6e457127cc4d92ea7058
jingeon <ephigh1397@naver.com>
2025-01-15 13:40:11 +0900

    feature: Create Users Entity

--- src/main/java/com/estsoft/exproject/entity/Users.java
+++ src/main/java/com/estsoft/exproject/entity/Users.java
@@ -1,18 +1,50 @@
 package com.estsoft.exproject.entity;
 
+import com.estsoft.exproject.enums.Membership;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
 public class Users {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "login_id", nullable = false)
     private String loginId;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String email;
+
+    @Column
+    private Membership membership;
+
+    @Column
+    private String phone;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "last_login_at")
+    private LocalDateTime lastLoginAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
 }
